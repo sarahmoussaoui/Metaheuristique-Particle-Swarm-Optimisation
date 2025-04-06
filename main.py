81d49bc80a4c4478f31e3800eb563115027cc764
sarah <moousarah@gmail.com>
2025-04-07 00:47:29 +0100

    added function for spt particles generation
    add parameter in optimize to choose if its a random swarm or an spt initialized swarm

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     dataset_path = "./src/data/processed/data_20j_15m/data_20j_15m_1.txt"  # Replace with your dataset path
     processor = JSSPProcessor(dataset_path)
     best_schedule, best_makespan, exec_time = processor.run(
-        num_particles=10, max_iter=50, w=0.7, c1=2.0, c2=1.0
+        num_particles=10, max_iter=50, w=0.7, c1=2.0, c2=1.0, use_spt=True
      )
