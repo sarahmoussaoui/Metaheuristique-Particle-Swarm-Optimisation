a25995eceb304a6c40f833a5d50d6ef4f0892773
Nabila Chehla Fekir <nabilachehlafekir@gmail.com>
2025-04-10 21:03:44 +0200

    📝 more grid

--- run_grid_search.py
+++ run_grid_search.py
@@ -3,13 +3,9 @@ from src.modules.gridSearch import PSOGridSearch
 
 def main():
     # Configuration
-    dataset_file = "src/data/processed/data_20j_15m/data_20j_15m_1.txt"  # Folder containing your JSSP instance files
-    output_params = (
-        "SPSO/paramaters/best_params_grid.json"  # Output file for best parameters
-    )
-    output_history = (
-        "SPSO/paramaters/search_history_grid.csv"  # Output file for search history
-    )
+    dataset_file = "src/data/processed/data_50j_15m/data_100j_20m_1.txt"  # Folder containing your JSSP instance files
+    output_params = "SPSO/paramaters/best_params_grid_100j_20m_1.json"  # Output file for best parameters
+    output_history = "SPSO/paramaters/search_history_grid_100j_20m_1.csv"  # Output file for search history
 
     # Initialize and run grid search
     grid_search = PSOGridSearch(
@@ -21,15 +17,15 @@ def main():
     # Optional: Customize parameter grid if needed
     grid_search.set_parameter_grid(
         {
-            "num_particles": [20, 30, 50],
+            "num_particles": [100, 150, 200],
             "max_iter": [1000],
-            "w": [(0.3, 0.8), (0.5, 0.9)],  # (min, max) tuples
-            "c1": [(0.2, 0.9), (0.4, 0.8)],  # (min, max) tuples
-            "c2": [(0.2, 0.85), (0.4, 0.9)],  # (min, max) tuples
-            "mutation_rate": [0.5, 0.3],
-            "max_stagnation": [5],
+            "w": [(0.2, 0.8), (0.4, 0.9)],  # (min, max) tuples
+            "c1": [(0.2, 0.9), (0.4, 0.9)],  # (min, max) tuples
+            "c2": [(0.2, 0.8), (0.3, 0.7)],  # (min, max) tuples
+            "mutation_rate": [0.5, 0.7],
+            "max_stagnation": [20],
             "early_stopping_window": [None],
-            "improvement_threshold": [0.08],
+            "improvement_threshold": [0.05],
         }
     )
     # Optional: Customize parameter grid if needed
@@ -40,7 +36,6 @@ def main():
     print("\nGrid search completed!")
     print(f"Best parameters saved to: {output_params}")
     print(f"Search history saved to: {output_history}")
-    print(f"Best average makespan: {results['best_avg_makespan']:.2f}")
 
 
 if __name__ == "__main__":
